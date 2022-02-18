d33694388836f0be2d2ca34c47d23fbb3adb25dc
Yash Sonawane <yash493031@gmail.com>
2022-02-18 18:24:37 +0530

    notification_done

--- lib/individual_screen.dart
+++ lib/individual_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 import 'constant.dart';
 
@@ -15,6 +20,66 @@ class IndividualScreen extends StatefulWidget {
 
 class _IndividualScreenState extends State<IndividualScreen> {
   TextEditingController msg = TextEditingController();
+
+  Future<void> getToken() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    String uid = user!.uid;
+
+    var _doc = await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(widget.snap[widget.index]['Info']['Uid'])
+        .get();
+    var _doc1 =
+        await FirebaseFirestore.instance.collection("Users").doc(uid).get();
+
+    bool docStatus = _doc.exists;
+    bool docStatus1 = _doc1.exists;
+    if (docStatus == true && docStatus1 == true) {
+      callOnFcmApiSendPushNotifications(
+          [_doc['Info']['Token']], msg.text, _doc1['Info']['Name']);
+      msg.clear();
+    }
+  }
+
+  callOnFcmApiSendPushNotifications(
+      List<String> userToken, String msg, String name) async {
+    print("Notification : ${userToken[0]} ${msg} - ${name}");
+
+    final postUrl = 'https://fcm.googleapis.com/fcm/send';
+    final data = {
+      "registration_ids": userToken,
+      "collapse_key": "type_a",
+      "notification": {
+        "title": name,
+        "body": msg,
+      }
+    };
+
+    final headers = {
+      'content-type': 'application/json',
+      'Authorization':
+          'Bearer 	AAAAxkegXuw:APA91bEAli9UzNE9IcqlM815UksVaU8c-18zEOLSMssEy2pYvPrIe1wH-zj-3e_oA7AZwiIXD8WzNKzyHa3MCHSHGrotegZMIPN2EqHnBr2XydMXZsIcR7IXBKxB5g1bf83uCT1ltQLS'
+    };
+
+    try {
+      final response = await http.post(Uri.parse(postUrl),
+          body: json.encode(data),
+          encoding: Encoding.getByName('utf-8'),
+          headers: headers);
+
+      if (response.statusCode == 200) {
+        // on success do sth
+        print('test ok push CFM');
+        return true;
+      } else {
+        print(' CFM error${response.reasonPhrase}');
+        // on failure do sth
+        return false;
+      }
+    } catch (e) {
+      print('exception$e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,32 +147,37 @@ class _IndividualScreenState extends State<IndividualScreen> {
                   ],
                 ),
                 SizedBox(height: 30),
-                Container(
-                  width: 107,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6.0),
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color(0xffFEC37B),
-                        Color(0xffFF4184),
-                      ],
+                InkWell(
+                  onTap: ()async{
+                    getToken();
+                  },
+                  child: Container(
+                    width: 107,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.0),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(0xffFEC37B),
+                          Color(0xffFF4184),
+                        ],
+                      ),
                     ),
-                  ),
-                  child: const Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Send',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 1),
-                          fontSize: 20,
-                          letterSpacing:
-                              0 /*percentages not used in flutter. defaulting to zero*/,
-                          fontWeight: FontWeight.normal,
-                          height: 1),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Send',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: 20,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.normal,
+                            height: 1),
+                      ),
                     ),
                   ),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,152 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:notification_app/home_page.dart';
 import 'package:notification_app/services/authentication_helper.dart';
 import 'package:notification_app/verification_screens/login_page.dart';
 
+Future<void> _messageHandler(RemoteMessage message) async {
+    print('background message ${message.notification!.body}');
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    // description: 'This channel is used for important notifications.', // description
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MyApp());
+  FirebaseMessaging.onBackgroundMessage(_messageHandler);
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  //Yash
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true, // Required to display a heads up notification
+    badge: true,
+    sound: true,
+  );
+
+  //LOcal Notification
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    // print('Got a message whilst in the foreground!');
+    // print('Message data: ${message.data}');
+    //
+    // if (message.notification != null) {
+    //   print('Message also contained a notification: ${message.notification}');
+    // }
+
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              // description: channel.description,
+              color: Colors.blue,
+              playSound: true,
+              icon: '@mipmap/ic_launcher',
+            ),
+          ));
+    }
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print('A new onMessageOpenedApp event was published!');
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+    if (notification != null && android != null) {
+      // showDialog(
+      //     context: context,
+      //     builder: (_) {
+      //       return AlertDialog(
+      //         title: Text(notification.title),
+      //         content: SingleChildScrollView(
+      //           child: Column(
+      //             crossAxisAlignment: CrossAxisAlignment.start,
+      //             children: [Text(notification.body)],
+      //           ),
+      //         ),
+      //       );
+      //     });
+      print("Body : " + notification.body!);
+    }
+  });
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  void showNotification(String title, String message) {
+    flutterLocalNotificationsPlugin.show(
+        0,
+        title,
+        message,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            // description: channel.description,
+            color: Colors.blue,
+            playSound: true,
+            icon: '@mipmap/ic_launcher',
+          ),
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    showNotification("Hey", "Welcome");
+
+      // print(AppSettings.pushNotification.toString());
+      FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+        print(event.notification!.body);
+      });
+      FirebaseMessaging.onMessageOpenedApp.listen((message) {
+        print('Message clicked!');
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser;
